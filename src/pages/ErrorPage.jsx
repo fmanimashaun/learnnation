08835350855469aee5ddee0853d262d04e8a8db8
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-16 07:28:56 +0100

    Update the tailwindcss

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 
 const ErrorPage = () => {
     return (
-        <article style={{ padding: "100px" }}>
+        <article>
             <h1>Oops!</h1>
             <p>Page Not Found</p>
-            <div className="flexGrow">
+            <div>
                 <Link to="/">Visit Our Homepage</Link>
             </div>
         </article>
