bdd7cc3096bb62959abbbd6b910cfdf5db0c6d34
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2024-02-19 16:40:00 +0100

    Task: working on contact form

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,11 @@
+
+
+const ContactForm = () => {
+  return (
+    <div className="w-2/5 h-96 mr-28 shadow-lg shadow-gray-800 rounded-md bg-slate-50">
+        <p>Send Message</p>
+    </div>
+  )
+}
+
+export default ContactForm
