d3820d59785676deee4c1aa87ec704c18732153d
Akukwe1000/facebook <akukwefavour1000@gmail.com>
2024-03-13 13:10:25 +0000

    testing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from 'react-icons/io';
 // import myImage from './myImage.jpg';
 import people from '../assets/imgs/people.png'
+import man from '../assets/imgs/man.png'
+import boy from '../assets/imgs/boy.png'
 
 
 const About = () => {
@@ -12,10 +14,11 @@ const About = () => {
         <h1>Decicated to achieving excellent in learning.</h1>
         <p>Discover the possibility of online education and unlock your true potential! expand your mind withonline education</p>
         <button>Get started today <IoIosArrowForward/> </button>
+
       </div>
       <div className="left">
-        <div> <img src={people} alt="My Image" /> </div>
-        <div></div>
+        <div> <img src={people} alt="My Image" /> <img src={man} alt="My Image" /> </div>
+        <div> <img src={boy} alt="My Image" /></div>
       </div>
     </section>
   );
